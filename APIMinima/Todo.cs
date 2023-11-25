b32c98b92e0ec3a655b9f69c911f7658d285284d
RosaBarco <rosa@tiredirect.com.mx>
2023-11-25 13:05:42 -0600

    Agregar archivos de proyecto.

---
+++ APIMinima/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace APIMinima
+{
+    public class Todo
+    {
+        public int id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
