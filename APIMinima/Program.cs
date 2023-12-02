cd8198bb767d739e9697a751903b3878ed0c0553
RosaBarco <rosa@tiredirect.com.mx>
2023-12-02 09:47:39 -0600

    Proyecto completo

--- APIMinima/Program.cs
+++ APIMinima/Program.cs
@@ -7,27 +7,111 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-app.MapGet("/todoitems", async (TodoDb db) =>
+/* var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/complete", GetCompleteTodos);
+todoItems.MapGet("/{id}", GetTodo);
+todoItems.MapPost("/", CreateTodo);
+todoItems.MapPut("/{id}", UpdateTodo);
+todoItems.MapDelete("/{id}", DeleteTodo); */
+
+RouteGroupBuilder todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/complete", GetCompleteTodos);
+todoItems.MapGet("/{id}", GetTodo);
+todoItems.MapPost("/", CreateTodo);
+todoItems.MapPut("/{id}", UpdateTodo);
+todoItems.MapDelete("/{id}", DeleteTodo);
+
+app.Run();
+
+static async Task<IResult> GetAllTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
+}
+
+static async Task<IResult> GetCompleteTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToArrayAsync());
+}
+
+static async Task<IResult> GetTodo(int id, TodoDb db)
+{
+    return await db.Todos.FindAsync(id)
+        is Todo todo
+        ? TypedResults.Ok(new TodoItemDTO(todo))
+        : TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodo(TodoItemDTO todoItemDto,TodoDb db)
+{
+    var todoItem = new Todo
+    {
+        IsComplete = todoItemDto.IsComplete,
+        Name = todoItemDto.Name
+    };
+
+    db.Todos.Add(todoItem);
+    await db.SaveChangesAsync();
+
+    todoItemDto = new TodoItemDTO(todoItem);
+
+    return TypedResults.Created($"/todoitems/{todoItem.id}", todoItemDto);
+}
+
+static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDto, TodoDb db)
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if(todo is null) return TypedResults.NotFound();
+
+    todo.Name = todoItemDto.Name;
+    todo.IsComplete = todoItemDto.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> DeleteTodo(int id, TodoDb db)
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+
+        TodoItemDTO todoItemDto = new TodoItemDTO(todo);
+
+        return TypedResults.Ok(todoItemDto);
+    }
+
+    return TypedResults.NotFound();
+}
+
+/*
+todoItems.MapGet("/", async (TodoDb db) =>
             await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
+todoItems.MapGet("/complete", async (TodoDb db) =>
             await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapGet("/{id}", async (int id, TodoDb db) =>
             await db.Todos.FindAsync(id)
             is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+todoItems.MapPost("/", async (Todo todo, TodoDb db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{todo.id}", todo);
+    return Results.Created($"/{todo.id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
     
@@ -45,7 +129,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 }
 );
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapDelete("/{id}", async (int id, TodoDb db) =>
 {
     if(await db.Todos.FindAsync(id) is Todo todo)
     {
@@ -57,6 +141,6 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
     return Results.NotFound();
 }
 );
+*/
 
-app.Run();
-  
+
--- APIMinima/Todo.cs
+++ APIMinima/Todo.cs
@@ -7,5 +7,7 @@
         public string? Name { get; set; }
 
         public bool IsComplete { get; set; }
+
+        public string? Secret { get; set; }
     }
 }
